ad268ee83f98b5bbf227fa90045e1df2db901f4e
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-06-10 23:36:06 -0300

    criados services, mappings, entities, controllers, o xml de configuração do nhibernate.. tá ficando top

---
+++ Vendinha-Samu.Backend/Vendinha-Samu.Api/Controllers/DividaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Vendinha_Samu.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DividaController : ControllerBase
+    {
+        // GET: api/<DividaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DividaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DividaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DividaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DividaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
