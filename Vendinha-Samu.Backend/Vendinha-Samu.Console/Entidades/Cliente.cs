13618cefb6404023f7c042853027ec2948f764b3
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-06-12 20:37:35 -0300

    pequenos ajustes nas entidades, agora a data de criação da dívida é setada para now e datapagamento deve ser nullable

--- Vendinha-Samu.Backend/Vendinha-Samu.Console/Entidades/Cliente.cs
+++ Vendinha-Samu.Backend/Vendinha-Samu.Console/Entidades/Cliente.cs
@@ -5,22 +5,22 @@ namespace Vendinha_Samu.Console.Entidades
     public class Cliente
     {
         [Required(ErrorMessage = "O Id é obrigatório")]
-        public int? Id { get; set; }
+        public int Id { get; set; }
 
         [Required(ErrorMessage = "O Nome é obrigatório")]
         [StringLength(50, ErrorMessage = "O Nome não pode passar de 50 caractéres")]
-        public string? NomeCompleto { get; set; }
+        public required string NomeCompleto { get; set; }
 
         [Required(ErrorMessage = "O CPF é obrigatório")]
         [RegularExpression(@"\d{11}", ErrorMessage = "O CPF deve conter exatamente 11 dígitos")]
-        public string? Cpf { get; set; }
+        public required string Cpf { get; set; }
 
         [Required(ErrorMessage = "A data de nascimento é obrigatória")]
-        public DateTime? DataNascimento { get; set; }
+        public required DateTime DataNascimento { get; set; }
 
         [Required(ErrorMessage = "O E-mail é obrigatório")]
         [EmailAddress(ErrorMessage = "O E-mail não é válido")]
         [StringLength(50, ErrorMessage = "O email não pode passar de 50 caractéres")]
-        public string? Email { get; set; }
+        public required string Email { get; set; }
     }
 }
--- Vendinha-Samu.Backend/Vendinha-Samu.Console/Entidades/Divida.cs
+++ Vendinha-Samu.Backend/Vendinha-Samu.Console/Entidades/Divida.cs
@@ -5,23 +5,23 @@ namespace Vendinha_Samu.Console.Entidades
     public class Divida
     {
         [Required(ErrorMessage = "O IdDivida é obrigatório")]
-        public int? IdDivida { get; set; }
+        public int IdDivida { get; set; }
 
         [Required(ErrorMessage = "O IdCliente é obrigatório")]
-        public int? IdCliente { get; set; }
+        public required int IdCliente { get; set; }
 
         [Required(ErrorMessage = "O Valor da dívida é obrigatório")]
-        public Decimal? Valor { get; set; }
+        public required Decimal Valor { get; set; }
 
         [Required(ErrorMessage = "A Data de Criação da dívida é obrigatória")]
-        public DateTime? DataCriacao { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
 
         [Required(ErrorMessage = "A situação da dívida é obrigatória")]
-        public bool Situacao { get; set; }
+        public required bool Situacao { get; set; }
         public DateTime? DataPagamento { get; set; }
 
         [Required(ErrorMessage = "A Descrição da dívida é obrigatória")]
         [StringLength(255, ErrorMessage = "A descrição não pode passar de 255 caractéres")]
-        public string? Descricao { get; set; }
+        public required string Descricao { get; set; }
     }
 }
