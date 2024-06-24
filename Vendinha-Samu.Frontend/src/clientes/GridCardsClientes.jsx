9d7d36f782f04135afac9d55ad015ffd4d250499
Samuel Pereira <samu.barreto2004@gmail.com>
2024-06-24 16:39:21 -0300

    estilização da barra de pesquisa refeita

--- Vendinha-Samu.Frontend/src/clientes/GridCardsClientes.jsx
+++ Vendinha-Samu.Frontend/src/clientes/GridCardsClientes.jsx
@@ -39,15 +39,10 @@ export default function GridCardsClientes() {
 
   return (
     <>
-      <div className="search-bar-container">
-        <input type="search" placeholder="Pesquise pelo Nome do Cliente" value={getBusca} onChange={(event) => {
-          setBusca(event.target.value);
-          setPage(1);
-        }} />
-        <svg xmlns="http://www.w3.org/2000/svg" className="base-icon black" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-        </svg>
-      </div>
+      <input type="search" placeholder="Pesquise pelo Nome do Cliente 🔍" value={getBusca} onChange={(event) => {
+        setBusca(event.target.value);
+        setPage(1);
+      }} />
 
       <main className="clients-grid" style={{
         gridTemplateColumns: getCards.length === 1 || getCards.length === 0
