178de876f035959ce2cd0ec549e757939c148b69
Samuel Pereira <samu.barreto2004@gmail.com>
2024-06-25 10:37:09 -0300

    botão debt sum cliente fica verde quando não deve nada

--- Vendinha-Samu.Frontend/src/clientes/CardCliente.jsx
+++ Vendinha-Samu.Frontend/src/clientes/CardCliente.jsx
@@ -43,7 +43,11 @@ export default function CardCliente(properties) {
         <p className="client-name-age">{cliente.nomeCompleto}, {idadeCliente}</p>
         <p className="client-email">{cliente.email}</p>
         {/* <Link to={`cliente/${cliente.id}/dividas`}> */}
-        <button className="debt-sum-client">R${(cliente.somatorioDividasEmAberto).toFixed(2)}
+        <button className="debt-sum-client" style={{
+          backgroundColor: cliente.somatorioDividasEmAberto == 0
+            ? 'var(--cor-verde)'
+            : 'var(--cor-vermelho)'
+        }}>R${(cliente.somatorioDividasEmAberto).toFixed(2)}
           <svg xmlns="http://www.w3.org/2000/svg" className="base-icon debt-svg" viewBox="0 0 16 16">
             <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
             <path d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2z" />
